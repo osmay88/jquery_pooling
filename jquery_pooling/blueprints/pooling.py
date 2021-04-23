4d1060210b3c8ccf2756d846fdde53c8cb243107
Osmay Cruz <osmay.cruz@gmail.com>
2021-04-23 12:44:57 -0400

    Added readme

--- jquery_pooling/blueprints/pooling.py
+++ jquery_pooling/blueprints/pooling.py
@@ -1,9 +1,6 @@
-from datetime import datetime
-
 from celery.result import AsyncResult
 from flask import request, jsonify
 from flask.blueprints import Blueprint
-from logging import getLogger
 
 bp = Blueprint("pooling_bp", "pooling_bp")
 
@@ -12,9 +9,6 @@ bp = Blueprint("pooling_bp", "pooling_bp")
 def pooling_view(*args, **kwargs):
     task_id = request.json["task_id"]
     result = AsyncResult(task_id)
-    if not result:
-        return "Not found", 404
-
     status = {
         "task_id": result.task_id,
         "status": result.status,
